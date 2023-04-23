01261c2e7fbb3e6e37cdb610489d5310cfdde6f9
Valerie <valchaulinux@gmail.com>
2023-04-22 17:04:10 -0700

    Add files via upload
    upldated Python code that creates 6 climate files to drop duplicate time/date rows so that this field can be used as a primary key in a sql database. 
    These rows appeared to be entirely duplicated across all columns. Also upldated database schema sql file to make this Date field into a timestamp field for Postgres

---
+++ database_schema.sql
@@ -0,0 +1,42 @@
+CREATE TABLE IF NOT EXISTS "WND" (
+    "Date" timestamp PRIMARY KEY NOT NULL,
+    "WND_direction" int   NOT NULL,
+    "WND_dQC" char(1)   NOT NULL,
+    "WND_type" char(1)   NOT NULL,
+    "WND_speed" int   NOT NULL,
+    "WND_speedQC" char(1)   NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS "SKY" (
+    "Date" timestamp PRIMARY KEY NOT NULL,
+    "SKY_ceiling" int   NOT NULL,
+    "SKY_ceilingQC" char(1)   NOT NULL,
+    "SKY_ceilingDC" char(1)   NOT NULL
+);
+
+CREATE TABLE  IF NOT EXISTS "VISIBILITY" (
+    "Date" timestamp PRIMARY KEY NOT NULL,
+    "VIS_distance" int   NOT NULL,
+    "VIS_distanceQC" char(1)   NOT NULL,
+    "VIS_variability" char(1)   NOT NULL,
+    "VIS_variabilityQC" char(1)   NOT NULL  
+);
+
+CREATE TABLE IF NOT EXISTS "AIR_TEMP" (
+    "Date" timestamp PRIMARY KEY NOT NULL,
+    "AIR_temp" int   NOT NULL,
+    "AIR_tempQC" char(1)   NOT NULL   
+);
+
+CREATE TABLE IF NOT EXISTS "AIR_DEW" (
+    "Date" timestamp PRIMARY KEY NOT NULL,
+    "AIR_dew" int   NOT NULL,
+    "AIR_dewQC" char(1)   NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS "AIR_PRESSURE" (
+    "Date" timestamp PRIMARY KEY NOT NULL,
+    "AIR_PRESSURE" int   NOT NULL,
+    "AIR_seapressQC" char(1)  NOT NULL   
+);
+
